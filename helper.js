7e234d46ebc335c17cc7ec13f4d53546550f00d9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-08-09 13:49:20 -0400

    20Vic Corporate 1.0  /helper.js

---
+++ helper.js
@@ -0,0 +1,29 @@
+function init() {
+    // Pausing the slick slider on mouseover
+    $.fn.slickPause = function() {
+        var _ = this;
+        return _.each(function(index, element) {
+            //start hack
+            var st = element.slick.$slideTrack.get(0);
+            element.slick.$slideTrack.css({
+                transition: '',
+                webkitTransform: window.getComputedStyle(st).webkitTransform,
+                transform: window.getComputedStyle(st).transform
+            });
+            element.slick.animating = false;
+        	//end hack
+        
+            element.slick.autoPlayClear();
+            element.slick.paused = true;
+        });
+    };
+
+
+    $( ".slick-slider" ).mouseover(function() {
+        $( ".paused" ).show();
+    });
+    
+    $( ".slick-slider" ).mouseout(function() {
+        $( ".paused" ).hide();
+    });
+}
